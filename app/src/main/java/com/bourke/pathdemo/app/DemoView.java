b04ac55017640a0b51a3c46c7b1137d3fba83498
Paul Bourke <pauldbourke@gmail.com>
2014-03-22 17:02:44 +0000

    Port DemoView to render SVG paths

--- app/src/main/java/com/bourke/pathdemo/app/DemoView.java
+++ app/src/main/java/com/bourke/pathdemo/app/DemoView.java
@@ -6,21 +6,37 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.DashPathEffect;
 import android.graphics.Paint;
-import android.graphics.Path;
 import android.graphics.PathEffect;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.LinearInterpolator;
 
 import com.bourke.pathdemo.app.SvgHelper.SvgPath;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DemoView extends View {
 
-    private final Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private static final String TAG = "PathDemo/DemoView";
 
-    private SvgPath mWaitPath;
+    private final Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private final SvgHelper mSvg = new SvgHelper(mPaint);
+
+    private ObjectAnimator mWaitAnimator;
+
+    private List<SvgPath> mPaths = new ArrayList<SvgPath>(0);
+
+    private Thread mLoader;
+    private final Object mSvgLock = new Object();
 
     private float mWait;
+
+    private int mDuration = 3000;
+    private float mFadeFactor = 10.0f;
+    private float mParallax = 1.0f;
 
     public DemoView(Context context, AttributeSet attrs) {
         this(context, attrs, 0);
@@ -32,34 +48,74 @@ public class DemoView extends View {
         init();
     }
 
+    @Override
+    protected void onSizeChanged(final int w, final int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+
+        Log.d(TAG, "onSizeChanged");
+
+        if (mLoader != null) {
+            try {
+                mLoader.join();
+            } catch (InterruptedException e) {
+                Log.e(TAG, "Unexpected error", e);
+            }
+        }
+
+        mLoader = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                mSvg.load(getContext(), R.raw.android_robot);
+                synchronized (mSvgLock) {
+                    mPaths = mSvg.getPathsForViewport(
+                            w - getPaddingLeft() - getPaddingRight(),
+                            h - getPaddingTop() - getPaddingBottom());
+                    updatePathsPhaseLocked();
+                }
+            }
+        }, "SVG Loader");
+        mLoader.start();
+    }
+
+    private void updatePathsPhaseLocked() {
+        final int count = mPaths.size();
+        for (int i = 0; i < count; i++) {
+            SvgHelper.SvgPath svgPath = mPaths.get(i);
+            svgPath.paint.setPathEffect(createPathEffect(svgPath.length, mWait, 0.0f));
+        }
+    }
+
     private void init() {
         mPaint.setStyle(Paint.Style.STROKE);
-        mPaint.setStrokeWidth(10.0f);
+        mPaint.setStrokeWidth(6.0f);
         mPaint.setColor(Color.rgb(164, 199, 57));
 
-        createWaitPath();
-
-        int mDuration = 4000;
-        ObjectAnimator mWaitAnimator = ObjectAnimator.ofFloat(this, "wait", 1.0f, 0.0f)
-                .setDuration(mDuration);
+        mWaitAnimator = ObjectAnimator.ofFloat(this, "wait", 1.0f, 0.0f).setDuration(mDuration);
         mWaitAnimator.setRepeatMode(ObjectAnimator.RESTART);
         mWaitAnimator.setRepeatCount(ObjectAnimator.INFINITE);
         mWaitAnimator.setInterpolator(new LinearInterpolator());
         mWaitAnimator.start();
     }
 
-    private void createWaitPath() {
-        Path p = new Path();
-        p.moveTo(32, 32);
-        p.lineTo(232, 32);
-
-        mWaitPath = new SvgHelper.SvgPath(p, mPaint);
-    }
-
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.drawPath(mWaitPath.path, mWaitPath.paint);
+
+        synchronized (mSvgLock) {
+            canvas.save();
+            canvas.translate(getPaddingLeft(), getPaddingTop());// + mOffsetY);
+            final int count = mPaths.size();
+            for (int i = 0; i < count; i++) {
+                SvgHelper.SvgPath svgPath = mPaths.get(i);
+
+                // We use the fade factor to speed up the alpha animation
+                int alpha = (int) (Math.min((1.0f - mWait) * mFadeFactor, 1.0f) * 255.0f);
+                svgPath.paint.setAlpha((int) (alpha * mParallax));
+
+                canvas.drawPath(svgPath.path, svgPath.paint);
+            }
+            canvas.restore();
+        }
     }
 
     public float getWait() {
@@ -68,13 +124,25 @@ public class DemoView extends View {
 
     public void setWait(float wait) {
         mWait = wait;
-        float targetLength = 32.0f;
-        mWaitPath.paint.setPathEffect(createPathEffect(mWaitPath.length, mWait, targetLength));
+        synchronized (mSvgLock) {
+            updatePathsPhaseLocked();
+        }
         invalidate();
     }
 
     private static PathEffect createPathEffect(float pathLength, float phase, float offset) {
         return new DashPathEffect(new float[] { pathLength, pathLength },
                 Math.max(phase * pathLength, offset));
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        switch(event.getAction()) {
+            case (MotionEvent.ACTION_UP) :
+                mWaitAnimator.start();
+                return true;
+            default :
+                return super.onTouchEvent(event);
+        }
     }
 }
